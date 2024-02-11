72f4f31f88c5f91bd84802adab4b4756a545da9c
Andrew Finogenov <avfinogenov@gmail.com>
2024-02-11 19:03:04 +0300

    initial comments

---
+++ unrusting/unrusting.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+
+
+// main task - create a first point of view 3d text graphics ( some kind of maze, that we can walk through)
+
+
+int main()
+{
+    //we need a person
+
+    // a map
+
+
+    //some window interaction
+
+    //game loop
+
+
+
+    //some paints
+
+
+
+
+    
+}
+
